d64a27788d02425b880caa581e71e4ca20b5b4b0
sridharrajs <me@sridhar.co>
2016-11-21 20:49:28 +0530

    base commit

--- configuration.js
+++ configuration.js
@@ -12,87 +12,20 @@ var urls = require('sdk/url');
 var tokensSeparator = ',';
 
 function get_credentials() {
-  return new Promise(function(resolve, reject) {
-    passwords.search({
-      username: prefs.wallabagUrl,
-      onComplete: function onComplete(credentials) {
-        if(credentials.length > 1) {
-          reject();
-        } else {
-          if(credentials.length === 1) {
-            var tokens = credentials[0].password.split(tokensSeparator);
-            resolve({
-              access_token: tokens[0],
-              refresh_token: tokens[1]
-            });
-          } else {
-            resolve(credentials);
-          }
-        }
-      }
+    return new Promise(function(resolve,reject){
+	var	token = 'asdfasdas';
+	resolve({
+	    token:token
+	});
     });
-  });
 }
 
-function reset_credentials() {
-  console.log('resetting credentials');
-  return new Promise(function(resolve, reject) {
-    passwords.search({
-      username: prefs.wallabagUrl,
-      onComplete: function onComplete(credentials) {
-        credentials.forEach(passwords.remove);
-        resolve(true);
-      }
+function set(token) {
+    passwords.store({
+	token:token,
+	realm: 'API Tokens'
     });
-  });
 }
 
-/**
- * Check if the configuration is ok for connection
- */
-function has_access(credentials) {
-  console.log(prefs.wallabagUrl, prefs.wallabagClientId, prefs.wallabagSecretId, credentials.access_token, credentials.refresh_token, urls.isValidURI(prefs.wallabagUrl));
-  return prefs.wallabagUrl && prefs.wallabagClientId && prefs.wallabagSecretId && credentials.access_token && credentials.refresh_token && urls.isValidURI(prefs.wallabagUrl);
-}
-
-/**
- * Check if configuration is ok for creating access token
- */
-function verify_config() {
-  if (prefs.wallabagUrl === "" || prefs.wallabagClientId === "" || prefs.wallabagSecretId === "") {
-    var errorMessage = _("cfg_required_msg");
-  } else if (!urls.isValidURI(prefs.wallabagUrl)) {
-    var errorMessage = _("cfg_valid_url_msg");
-  }
-
-  if (errorMessage) {
-    notifications.notify({
-      title: _("cfg_config_error_title"),
-      text: errorMessage,
-      iconURL: "./icon-64.png"
-    });
-
-    preferencesUtils.open(self);
-    return reject();
-  } else {
-    return resolve({
-      url: prefs.wallabagUrl.replace(/\/$/, ""),
-      client_id: prefs.wallabagClientId,
-      client_secret: prefs.wallabagSecretId
-    });
-  }
-}
-
-function set(wallabag_url, wallabag_access_token, wallabag_refresh_token) {
-  passwords.store({
-    username: wallabag_url,
-    password: wallabag_access_token + tokensSeparator + wallabag_refresh_token,
-    realm: 'API Tokens'
-  });
-}
-
-exports.has_access = has_access;
-exports.verify_config = verify_config;
 exports.set = set;
 exports.get_credentials = get_credentials;
-exports.reset_credentials = reset_credentials;
--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const { defer } = require('sdk/core/promise');
 var Request = require("sdk/request").Request;
 
-function create(url, client_id, client_secret, access_token, refresh_token) {
-  return {
-    url: url,
-    client_id: client_id,
-    client_secret: client_secret,
-    access_token: access_token,
-    refresh_token: refresh_token
-  };
-}
-
 function post(server, url) {
   var deferred = defer();
-  console.log("I'm gonna add " + url + " on the " + server.url + "/api/entries!");
 
   Request({
-    url: server.url + "/api/entries",
+    url: server.url + "/api/articles",
     content: {
-      url: url,
+      url: url
     },
     headers: {
       'Authorization': "Bearer " + server.access_token
@@ -35,5 +24,4 @@ function post(server, url) {
   return deferred.promise;
 }
 
-exports.create = create;
 exports.post = post;
